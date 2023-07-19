43ce5e67eef84fae399018f7e4b91f22e0b26b8d
pankaj114425 <115251359+pankaj114425@users.noreply.github.com>
2023-07-19 11:33:17 +0530

    final code
    deployment of project

---
+++ server.js
@@ -0,0 +1,44 @@
+ const express = require('express')
+ const colors=require('colors')
+ const moragan=require('morgan')
+ const dotenv=require('dotenv');
+const connectDB = require('./config/db');
+ const path=require('path');
+// dotnev config
+dotenv.config({path: 'routes/.env'});
+//mongodb connection
+ connectDB();
+//rest object
+const app=express()
+
+//middlewares
+app.use(express.json())
+app.use(moragan('dev'))
+
+//routes
+// app.get("/",(req,res)=>{
+//     res.status=(200).send({
+//         message:"server running",
+//     });
+// });  purpose for test 
+
+//we add main route of user 
+app.use('/api/v1/user',require('./routes/userRoutes'));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+
+//static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// port
+const port =process.env.PORT || 8080
+//listen
+app.listen(port,()=>{
+    console.log(
+        `server running in ${process.env.NODE_MODE} mode on port ${process.env.PORT}`.bgCyan.white
+        );
+});
